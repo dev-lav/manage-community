f2ac497630536e91109eb120332f7f6d735138a1
Jaylani <leejungjay1124@gmail.com>
2019-10-09 05:43:05 +0700

    Add prefix for api routes

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::group(['prefix' => 'v1'], function(){
 
-	Route::get('positions', 
-		'Api\V1\PositionController@getListPosition'
-	);
-	Route::post('positions',
-		'Api\V1\PositionController@createPositionData'
-	);
-	Route::get('positions/{positionId}',
-		'Api\V1\PositionController@getPositionById'
-	);
-	Route::put('positions/{positionId}',
-		'Api\V1\PositionController@updateSinglePositionData'
-	);
-	Route::delete('positions/{positionId}',
-		'Api\V1\PositionController@deleteSinglePositionData'
-	);
-	
+		Route::get('positions', 
+			'Api\V1\PositionController@getListPosition'
+		);
+		Route::post('positions',
+			'Api\V1\PositionController@createPositionData'
+		);
+		Route::get('positions/{positionId}',
+			'Api\V1\PositionController@getPositionById'
+		);
+		Route::put('positions/{positionId}',
+			'Api\V1\PositionController@updateSinglePositionData'
+		);
+		Route::delete('positions/{positionId}',
+			'Api\V1\PositionController@deleteSinglePositionData'
+		);
+
 });
