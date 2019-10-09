c20fa0e754fe08a14c4795832e04e3e5843f22f9
Jaylani <leejungjay1124@gmail.com>
2019-10-09 22:20:43 +0700

    changed community model

---
+++ app/Community.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Community extends Model
+{
+    /**
+    * Define table name
+    * @var string
+    */
+    protected $table = 'communities';
+
+  	/**
+  	*
+  	* list of fillable column
+  	* @var array
+  	*/
+  	protected $fillable = [
+  		'id',
+      'name',
+  		'email',
+  		'password',
+  		'vission',
+  		'mission',
+  		'description',
+  	];
+
+    /**
+    * Eloquent relationship for this model 
+    * with crew model  
+    * One to Many relationship
+    * @return void
+    */
+    public function crews(){
+      return $this->hasMany('App\Crew', 'community_id');
+    }
+
+}
