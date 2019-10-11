864a07cb4e34dcc37a3de9e821e08440550d8ca0
Mathews Lima <mathews.limaa@gmail.com>
2019-10-11 16:55:57 -0300

    [fix] migration - remove the reference method, because the table venues don't exist

--- database/migrations/2019_10_10_002553_create_events_table.php
+++ database/migrations/2019_10_10_002553_create_events_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateEventsTable extends Migration
 {
@@ -24,16 +24,10 @@ class CreateEventsTable extends Migration
             $table->softDeletes();
 
             $table
-            ->foreign('crew_id')
-            ->references('id')
-            ->on('crews')
-            ->onDelete('cascade');
-
-            $table
-            ->foreign('venue_id')
-            ->references('id')
-            ->on('venues')
-            ->onDelete('cascade');
+                ->foreign('crew_id')
+                ->references('id')
+                ->on('crews')
+                ->onDelete('cascade');
         });
     }
 
