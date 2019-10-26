8ea50b41d6f0989670b6ab22a504480bed490cad
fatoni <fatoniahmad.mail@gmail.com>
2019-10-26 21:15:57 +0700

    refactor: routes, controller, validation

--- app/Http/Controllers/Api/V1/CrewController.php
+++ app/Http/Controllers/Api/V1/CrewController.php
@@ -5,60 +5,68 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\CrewResource;
+use App\Http\Requests\Crew\StoreCrewValidator;
 use App\Crew;
 
 class CrewController extends Controller
 {
 
-    public function getListCrew(){
-    	
-    	return CrewResource::collection(
-    			     Crew::paginate()
-    		);
-
+    /**
+     * get list paginate
+     * @return JSON
+     */
+    public function index(){
+    	return $this->apiResponseBuilder(200, Crew::paginate() );
     }
 
-    public function createCrewData(Request $request){
-
-    	$attributes = $request->json()->all();
-    	
-    	Crew::create($attributes);
-
-    	return response(
-    			new CrewResource(
-    				Crew::latest()->first())
-    		);
+    /**
+     * store new data to database
+     * @param  Request $request 
+     * @return JSON
+     */
+    public function create(Request $request, StoreCrewValidator $validator){
+        $data = $request->json()->all();
+        $validation = $validator->validate($data);
+        if ($validation === true) {
+            $newData = Crew::create($data);
+            return $this->apiResponseBuilder(200,  $newData);
+        }
+        return $this->apiUnprocessableEntityResponse($validation->all());
     }
 
-    public function getCrewById($crewId){
-
-        return response(
-            new CrewResource(
-                    Crew::findOrFail($crewId)
-            )
-        );
+    /**
+     * show detail data
+     * @param  int $crewId 
+     * @return JSON
+     */
+    public function show($crewId){
+        return $this->apiResponseBuilder(200, Crew::find($crewId));
     }
 
-    public function updateSingleCrewData(Request $request, $crewId){
-
-        $attributes = $request->json()->all();
-        
-        Crew::findOrFail($crewId)
-                   ->update($attributes);
-
-        return response(
-            new CrewResource(
-                Crew::findOrFail($crewId)
-            )
-        );            
+    /**
+     * update selected data
+     * @param  Request $request
+     * @param  int  $crewId 
+     * @return JSON
+     */
+    public function update(Request $request, $crewId){
+        $data = $request->json()->all();
+        $validator = new StoreCrewValidator($crewId);
+        $validation = $validator->validate($data);
+        if ($validation === true) {
+            $updatedData = Crew::find($crewId)->update($data);
+            return $this->apiResponseBuilder(200,  Crew::find($crewId));
+        }
+        return $this->apiUnprocessableEntityResponse($validation->all());      
     }
 
-    public function deleteSingleCrewData($crewId){
-    	
-        Crew::findOrFail($crewId)
-                       ->delete();   
-
-        return response()->json("successfully deleted!");
-
+    /**
+     * delete selected data
+     * @param  int $crewId 
+     * @return JSON
+     */
+    public function delete($crewId){
+    	Crew::find($crewId)->delete();
+        return response()->json('Data has been deleted');
     }
 }
